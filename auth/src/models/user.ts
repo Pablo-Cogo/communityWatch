51550a7363518927128222ffa45940beed67d725
Pablo-Cogo <pablocogo1@gmail.com>
2023-08-19 11:32:50 -0300

    AuthService, bcrypt and tests

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document } from 'mongoose';
+import { AuthService } from '@src/services/auth';
 
 export interface User {
   _id?: string;
@@ -6,6 +7,10 @@ export interface User {
   userEmail: string;
   userPassword: string;
   userImage?: string;
+}
+
+export enum CUSTOM_VALIDATION {
+  DUPLICATED = 'DUPLICATED',
 }
 
 interface UserModel extends Omit<User, '_id'>, Document {}
@@ -19,7 +24,7 @@ const schema = new mongoose.Schema(
       unique: true,
     },
     userPassword: { type: String, required: true },
-    // userImage: {type: string}
+    userImage: { type: String, required: false },
   },
   {
     toJSON: {
@@ -31,5 +36,27 @@ const schema = new mongoose.Schema(
     },
   }
 );
+
+schema.path('userEmail').validate(
+  async (userEmail: string) => {
+    const emailCount = await mongoose.models.User.countDocuments({ userEmail });
+    return !emailCount;
+  },
+  'already exists in the database.',
+  CUSTOM_VALIDATION.DUPLICATED
+);
+
+schema.pre<UserModel>('save', async function (): Promise<void> {
+  if (!this.userPassword || !this.isModified('userPassword')) {
+    return;
+  }
+
+  try {
+    const hashedPassword = await AuthService.hashPassword(this.userPassword);
+    this.userPassword = hashedPassword;
+  } catch (error) {
+    console.error(`Error hashing password for the user ${this.userName}`);
+  }
+});
 
 export const User = mongoose.model<UserModel>('User', schema);
--- auth/test/functional/users.test.ts
+++ auth/test/functional/users.test.ts
@@ -1,8 +1,12 @@
 import { User } from '@src/models/user';
+import { AuthService } from '@src/services/auth';
 
 describe('users functional tests', () => {
+  beforeEach(async () => {
+    await User.deleteMany();
+  });
   describe('when creating a new user', () => {
-    it('should sucessfully a create new user', async () => {
+    it('should sucessfully a create new user with encrypted password', async () => {
       const newUser: User = {
         userName: 'Elon Musk',
         userEmail: 'elonMusk@mail.com',
@@ -11,7 +15,51 @@ describe('users functional tests', () => {
 
       const response = await global.testRequest.post('/users').send(newUser);
       expect(response.status).toBe(201);
-      expect(response.body).toEqual(expect.objectContaining(newUser));
+
+      await expect(
+        AuthService.comparePassword(
+          newUser.userPassword,
+          response.body.userPassword
+        )
+      ).resolves.toBeTruthy();
+
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          ...newUser,
+          ...{ userPassword: expect.any(String) },
+        })
+      );
+    });
+
+    it('should return 422 when there is validation error', async () => {
+      const newUser = {
+        userEmail: 'elonMusk@mail.com',
+        userPassword: '1234',
+      };
+
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        code: 422,
+        error: 'User validation failed: userName: Path `userName` is required.',
+      });
+    });
+
+    it('should return 409 when email already exists', async () => {
+      const newUser: User = {
+        userName: 'Elon Musk',
+        userEmail: 'elonMusk@mail.com',
+        userPassword: '1234',
+      };
+      await global.testRequest.post('/users').send(newUser);
+      const response = await global.testRequest.post('/users').send(newUser);
+      expect(response.status).toBe(409);
+      expect(response.body).toEqual({
+        code: 409,
+        error:
+          'User validation failed: userEmail: already exists in the database.',
+      });
     });
   });
 });
