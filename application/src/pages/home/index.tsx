62f59719614635488205458dddce5fd6c420c97a
Luan Bagio Compagnoni <luancmpg@gmail.com>
2023-12-01 21:47:47 -0300

    initilized dev of map

--- application/src/pages/home/index.tsx
+++ application/src/pages/home/index.tsx
@@ -1,12 +1,21 @@
+import { useLoadScript } from "@react-google-maps/api";
 import { Button } from "../../components/atoms/Button";
 import ExternalHeader from "../../components/organisms/header/external";
 import { Container } from "../../components/organisms/header/external/style";
+import Map from "../../components/organisms/map";
+import image from "../../assets/walpaper/walpaper-home.jpg";
 
 const Home = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyB6G-WhrxCon1Blj9gMxjkTX19AhEnkkU8',
+    libraries: ["places"],
+  });
+  if (!isLoaded) return <div>Loading...</div>;
   return (
     <section className="h-full">
       <ExternalHeader />
       <main className="flex items-center w-full h-full bg-black pt-[100px] max-[780px]:pt-[20%]">
+      <img src={image} alt="" className="h-full object-cover absolute w-full top-0" />
         <Container className="w-full flex justify-between items-start py-[20px] relative">
           {/* <span
             style={{ fontSize: "70px" }}
@@ -18,6 +27,9 @@ const Home = () => {
           <span className="w-[50%] h-[100%] rounded-[10px] bg-white flex items-center justify-center">
             alguma imagem...
           </span> */}
+          
+            {/* <Map /> */}
+
         </Container>
       </main>
     </section>
